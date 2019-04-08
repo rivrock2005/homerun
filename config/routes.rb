b30184153b0030e1d2616a2242081135ee388974
Chris Ashton <ashton.chris33@gmail.com>
2019-04-08 12:17:19 -0700

    lesson 4.1

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   resources :products
   get 'simple_pages/about'
   get 'simple_pages/contact'
-  get 'simple_pages/products'
   get 'simple_pages/index'
   root 'simple_pages#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
