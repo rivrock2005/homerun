c3719592ddeec7061391410cfc64147ed9ed7aeb
Chris Ashton <ashton.chris33@gmail.com>
2019-04-11 13:30:50 -0700

    lesson add price to product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -22,7 +22,7 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-  params.require(:product).permit(:name, :description, :image_url, :color)
+  params.require(:product).permit(:name, :description, :image_url, :color, :price)
 end
 
 
@@ -74,6 +74,6 @@ end
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def product_params
-      params.require(:product).permit(:name, :description, :image_url)
+      params.require(:product).permit(:name, :description, :image_url, :color, :price)
     end
 end
--- app/controllers/simple_pages_controller.rb
+++ app/controllers/simple_pages_controller.rb
@@ -4,4 +4,7 @@ class SimplePagesController < ApplicationController
 
   def contact
   end
+
+  def Products
+  end
 end
