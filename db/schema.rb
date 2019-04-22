5246bbfe59a1cb1586861f77f1f13e3126f57a33
Chris Ashton <ashton.chris33@gmail.com>
2019-04-22 09:14:49 -0700

    revision 3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_15_164954) do
+ActiveRecord::Schema.define(version: 2019_04_17_190653) do
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
