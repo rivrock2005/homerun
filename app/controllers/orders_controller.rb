cf638da95ac66162cd715764c3156284fb06630f
Chris Ashton <ashton.chris33@gmail.com>
2019-05-02 11:56:04 -0700

    lesson 4.6

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,12 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders = Order.includes(:product).all
     end
 
     def show
+      @order = Order.find(params[:id])
+
     end
 
     def new
