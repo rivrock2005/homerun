b3f5a901a8a6e1a2581ac1e8c4c7150745abe863
Chris Ashton <ashton.chris33@gmail.com>
2019-05-10 13:35:46 -0700

    lesson 5.2 revision 1

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,13 +1,17 @@
 class Product < ApplicationRecord
-has_many :orders
+	has_many :orders
 
-validates :image_url, allow_blank: true, format: {
-  with: %r{\.gif|jpg|png}i,
-  message: 'must be a url for gif, jpg, or png image.'
-}
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.gif|jpg|png}i,
+		message: 'must be a url for gif, jpg, or png image.'
+	}
 
-end
+	def self.search(search_term)
+		if Rails.env.development?
+			Product.where("name LIKE ?", "%#{search_term}%")
+		else Rails.env.production?
+			Product.where("name ilike ?", "%#{search_term}%")
+		end
+	end
 
-def self.search(search_term)
-  Product.where("name LIKE ?", "%#{search_term}%")
 end
