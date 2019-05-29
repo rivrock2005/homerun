2f1006706f2ff6a74ca3280f145cdb3cbcf8ef3a
Chris Ashton <ashton.chris33@gmail.com>
2019-05-29 07:48:39 -0700

    lesson 5.7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,16 @@
 class Product < ApplicationRecord
 	has_many :orders
+	has_many :comments
 
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.gif|jpg|png}i,
 		message: 'must be a url for gif, jpg, or png image.'
 	}
+
+	def highest_rating_comment
+  comments.rating_desc.first
+end
+
 
 	def self.search(search_term)
 		if Rails.env.development?
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_17_171955) do
+ActiveRecord::Schema.define(version: 2019_05_29_140014) do
+
+  create_table "comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.integer "rating"
+    t.integer "product_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_comments_on_product_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "orders", force: :cascade do |t|
     t.integer "user_id"
@@ -28,6 +39,12 @@ ActiveRecord::Schema.define(version: 2019_05_17_171955) do
     t.datetime "updated_at", null: false
     t.string "color"
     t.integer "price"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "review"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
