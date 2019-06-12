6701a0f79a2d6cc0a732976566d2d93ff56b9b3e
Chris Ashton <ashton.chris33@gmail.com>
2019-06-12 11:50:20 -0700

    lesson 5.9

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,8 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 
+	validates :name, presence: true
+
 	validates :image_url, allow_blank: true, format: {
 		with: %r{\.gif|jpg|png}i,
 		message: 'must be a url for gif, jpg, or png image.'
