7ad73662a1d6806638dba86071073fbbe705d0a0
Chris Ashton <ashton.chris33@gmail.com>
2019-06-13 16:29:48 -0700

    lesson 5.9 revision 2 after our call

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -17,8 +17,8 @@ class ProductsController < ApplicationController
  # GET /products/1.json
  def show
      @comments = @product.comments.order('created_at DESC')
-     @comments = @product.comments.paginate(page: params[:page], per_page: 2)
-     
+     @comments = @product.comments.paginate(page: params[:page], per_page: 5)
+
    end
 
  # GET /products/new
