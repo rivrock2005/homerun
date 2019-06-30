11462a499a238c0d8d4e8bdc98af07b82c9625f9
Chris Ashton <ashton.chris33@gmail.com>
2019-06-30 16:23:39 -0700

    lesson 6.4 revision 1

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -3,20 +3,22 @@ require 'rails_helper'
 
 
 describe UsersController, type: :controller do
-   let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
-   @user = FactoryBot.create(:user)
+ let(:user) { User.create!(email: 'peter@example.com', password: '1234567890') }
+ @user = FactoryBot.create(:user)
 
-  describe 'GET #show' do
-     context 'when a user is logged in' do
+describe 'GET #show' do
+   context 'when a user is logged in' do
+
+   end
+
+
+  context 'when a user is not logged in' do
+     it 'redirects to login' do
+       get :show, params: { id: user.id }
+       expect(response).to redirect_to(root_path)
 
      end
-
-     context 'when a user is not logged in' do
-       it 'redirects to login' do
-         get :show, params: { id: user.id }
-         expect(response).to redirect_to(root_path)
-       end
-     end
-  end
+   end
+end
 
 end
