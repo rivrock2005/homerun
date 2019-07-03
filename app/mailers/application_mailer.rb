cd84c9ad72579771a6f3feee21ff44b184780e1c
Chris Ashton <ashton.chris33@gmail.com>
2019-07-03 16:00:03 -0700

    lesson 6.5 revision 2

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -2,7 +2,6 @@ class ApplicationMailer < ActionMailer::Base
   default from: 'ashton.chris33@gmail.com'
   layout 'mailer'
 
-  
-    subject: "Welcome to My Awesome Site", body: 'I am the email body.').deliver_now
+    
 
   end
