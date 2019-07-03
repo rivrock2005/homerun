fe1fbbe8f08e7728c612e9523ef96988883a0433
Chris Ashton <ashton.chris33@gmail.com>
2019-07-03 16:35:12 -0700

    lesson 6.6

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -2,16 +2,28 @@ class PaymentsController < ApplicationController
 end
 
 def create
+  @product params[:product_id]
+  @user params [user_signed_in?]
   token = params[:stripeToken]
   # Create the charge on Stripe's servers - this will charge the user's card
   begin
     charge = Stripe::Charge.create(
-      amount: 2000, # amount in cents, again
+      amount="<%= @product %>"
       currency: "usd",
       source: token,
       description: params[:stripeEmail]
     )
-  rescue Stripe::CardError => e
-    # The card has been declined
+      if charge.paid
+        Order.create()
   end
+
+rescue Stripe::CardError => e
+
+
+
+
+rescue Stripe::CardError => e
+  # The card has been declined
+  redirect_to product_path(@product)
+end
 end
